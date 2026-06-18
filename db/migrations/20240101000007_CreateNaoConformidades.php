6032793f2fe322a0a493cd2d64c45a5d7db29d39
Lucas Borges Vargas <lucaskl.borgesvargas@gmail.com>
2026-06-18 20:01:16 -0300

    feat: migration nao_conformidades

---
+++ db/migrations/20240101000007_CreateNaoConformidades.php
@@ -0,0 +1,26 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateNaoConformidades extends AbstractMigration
+{
+    public function up(): void
+    {
+        $this->execute("
+            CREATE TABLE nao_conformidades (
+                id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                id_entrega INT UNSIGNED NOT NULL,
+                id_motivo  INT UNSIGNED NOT NULL,
+                descricao  VARCHAR(500) NULL,
+                CREATED_AT DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                CONSTRAINT id_entrega FOREIGN KEY (id_entrega) REFERENCES entregas (id),
+                CONSTRAINT id_motivo FOREIGN KEY (id_motivo) REFERENCES motivos_nao_conformidade (id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+        ");
+    }
+
+    public function down(): void
+    {
+        $this->execute('DROP TABLE IF EXISTS nao_conformidades');
+    }
+}
